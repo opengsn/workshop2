aa69fba2b878dcc805909981be2b8aad501d4b38
Dror Tirosh <dror@opengsn.org>
2021-06-07 22:43:27 +0300

    Adding GSN support
    add GSN provider support
    
    minimal set of changes to add GSN support to the contract and the dApp
    so that calling user no longer needs eth.
    
    Actual payment is done by the default "accept everything" paymaster
    contract.

--- contracts/CaptureTheFlag.sol
+++ contracts/CaptureTheFlag.sol
@@ -3,16 +3,24 @@
  */
 pragma solidity ^0.7.6;
 
-contract CaptureTheFlag {
+import "@opengsn/contracts/src/BaseRelayRecipient.sol";
+
+contract CaptureTheFlag is BaseRelayRecipient {
 
     event FlagCaptured(address previousHolder, address currentHolder);
 
     address public currentHolder = address(0);
 
+    constructor(address forwarder) {
+      trustedForwarder = forwarder;
+    }
+
+    string public override versionRecipient = "2.2.0";
+
     function captureTheFlag() external {
         address previousHolder = currentHolder;
 
-        currentHolder = msg.sender;
+        currentHolder = _msgSender();
 
         emit FlagCaptured(previousHolder, currentHolder);
     }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const CaptureTheFlag = artifacts.require('CaptureTheFlag')
 
+
 module.exports = async function (deployer) {
-  await deployer.deploy(CaptureTheFlag)
+  const forwarder = require( '../build/gsn/Forwarder' ).address
+  await deployer.deploy(CaptureTheFlag, forwarder)
+
+  console.log( `Deployed CTF at ${CaptureTheFlag.address} with forwarder ${forwarder}`)
 }
